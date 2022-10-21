0df2910a7e763e4cbb09e017f569ed5d9d546499
btwoa <109867618+btwoa@users.noreply.github.com>
2022-10-21 18:12:13 +0800

    ⭐

---
+++ source/demo/index.md
@@ -0,0 +1,8 @@
+---
+title: 测试
+layout: demo
+theme: flip
+password: DDDemo3
+---
+
+![](https://img.btwoa.com/eiruavbjlsdznkliorhesdfeievbsdfkievbfdjkevvejsblke.jpg)
---
+++ themes/apollo/source/js/console.js
@@ -0,0 +1,12 @@
+if(window.console){Function.prototype.makeMulti=function(){let l=new String(this);l=l.substring(l.indexOf("/*")+3,l.lastIndexOf("*/"));return l};let string=function(){
+/*
+
+      ██████╗ ████████╗██╗    ██╗ ██████╗  █████╗ 
+      ██╔══██╗╚══██╔══╝██║    ██║██╔═══██╗██╔══██╗
+      ██████╔╝   ██║   ██║ █╗ ██║██║   ██║███████║
+      ██╔══██╗   ██║   ██║███╗██║██║   ██║██╔══██║
+      ██████╔╝   ██║   ╚███╔███╔╝╚██████╔╝██║  ██║
+      ╚═════╝    ╚═╝    ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝
+                                                                                     
+*/
+};console.log(string.makeMulti());console.log("%c良宴玉食呼客来 盈门亲故共暄寒 佳节最是难尽欢 覆觞止酒空余啽", "color:#dbbdfd;font-weight:bold")}                            
